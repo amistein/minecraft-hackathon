62ae101666399399508e2dac8bbb69d0adab671d
Yoo-Nah Park <ypark2270@gmail.com>
2016-11-21 17:38:48 -0500

    GOT IT WORKING!!!

--- recorder.js
+++ recorder.js
@@ -1,4 +1,8 @@
 var events = require('events');
+var teleport = require('teleport');
+var utils = require('utils');
+var yoonahbrow = utils.player('yoonahbrow');
+
 console.log("S.P.I.C.E. TEAM YEAH!");
 // var myBlockBreakListener = events.on( Packages.net.canarymod.hook.player.BlockDestroyHook, function( evt ) {
 
@@ -9,24 +13,57 @@ console.log("S.P.I.C.E. TEAM YEAH!");
 
 var eventsArr = [];
 var isRecording = false;
+var speed = 100;
 
 events.playerMove(function(evt){
   if(isRecording){
     eventsArr.push(evt);
   }
   console.log(evt.player.name);
+  console.log(evt.from);
+  console.log(evt.to);
 
 });
 
 exports.startRecord = function(){
+  if(isRecording){
+    console.log("Already recording!");
+  }
+  eventsArr = [];
   isRecording = true;
 }
 
-exports.playback = function(){
-  console.log("hi");
+var nextMove = function (arr, n){
+    if(n>=arr.length) return;
+
+    teleport(yoonahbrow, arr[n].from);
+
+    setTimeout(function(){
+      nextMove(arr, n+1);
+    }, speed);
+};
+
+exports.playback = function(newSpeed){
+  if(!isRecording){
+    speed = newSpeed;
+    nextMove(eventsArr, 0);
+  }
+  else{
+    console.log("Stop recording before playback");
+  }
 }
 
 exports.stopRecord = function(){
+  if(!isRecording){
+    console.log("Already NOT recording!");
+  }
   isRecording = false;
 }
+
+// exports.originalSpawn = function(){
+//   teleport(yoonahbrow, eventsArr[0].from);
+// }
+
+
+
 //command r = to get the recorder started
